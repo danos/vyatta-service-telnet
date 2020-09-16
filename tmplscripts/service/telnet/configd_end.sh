de3061769b8d967ee1e78db6f06a86ab2620964b
Charles (Chas) Williams <ciwillia@vyatta.att-mail.com>
2020-09-16 18:30:57 -0400

    debian: use transform to edit system files
    /etc/securetty is updated as part of VRVDR-36556 when implementing
    allow-root. We need to deny root logins unless allow-root is configured.
    configd:end always runs when the configuration is first loaded and
    removes the vendor configuration. However, it's smarter to just do this
    once when this package is installed.
    
    Bugfix: VRVDR-53067

--- tmplscripts/service/telnet/configd_end.sh
+++ tmplscripts/service/telnet/configd_end.sh
@@ -1,5 +1,4 @@
 #!/opt/vyatta/bin/cliexec
-sed -i -e '/^# Unix98 PTY slaves/,/#.../d' /etc/securetty
 if [[ ${COMMIT_ACTION} != "DELETE" ]]; then
     /opt/vyatta/sbin/vyatta_update_telnet --vrf=$VAR(../../../routing-instance/@) --operation=enable --port=$VAR(port/@) --address=$VAR(listen-address/@)
 fi
